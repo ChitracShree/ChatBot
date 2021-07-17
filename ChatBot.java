87f1cdd843301515f9159427fe44e1e80fe664e7
Chitra Shree <87510824+ChitracShree@users.noreply.github.com>
2021-07-17 05:35:39 +0530

    Add files via upload

---
+++ ChatBot.java
@@ -0,0 +1,140 @@
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.awt.event.KeyEvent;
+
+import java.lang.Math;
+
+public class ChatBot extends JFrame implements KeyListener{
+
+	JPanel p=new JPanel();
+	JTextArea dialog=new JTextArea(20,50);
+	JTextArea input=new JTextArea(1,50);
+	JScrollPane scroll=new JScrollPane(
+		dialog,
+		JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+		JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
+	);
+	
+	String[][] chatBot={
+		//standard greetings
+		{"hi","hello","hola","ola","howdy","hey"},
+		{"hi","hello","hey"},
+		//question greetings
+		{"how are you","how r you","how r u","how are u","How are you"},
+		{"good","doing well"},
+		//where questions
+		{"where are you","whr r u","where r you","where are u","whr r u"},
+		{"Infront of you!!","In your computer","I don't know","Only my master knows"},
+		//combined questions
+		{"Hi,How are you","hi,How are you","hi,how are you","Hi,How r u","Hi,how are u","hi,how r u"},
+		{"Hi,I am fine","Hello, I am fine","Hi,I am good","Hello,I am good"},
+		//yes
+		{"yes"},
+		{"no","NO","NO!!!!!!!","Maybe!!"},
+		//bye
+		{"Bye","Good bye","Gud bye","bye","Good Bye","See you soon","good bye"},
+		{"Bye","See you soon","Have a great time"},
+		//default
+		{"shut up","you're bad","noob","stop talking",
+		"(Bot is (+_+))"}
+	};
+	
+	public static void main(String[] args){
+		new ChatBot();
+	}
+	
+	public ChatBot(){
+		super("Chat Bot");
+		setSize(600,400);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		dialog.setEditable(false);
+		input.addKeyListener(this);
+	
+		p.add(scroll);
+		p.add(input);
+		p.setBackground(new Color(102,0,153));
+		add(p);
+		
+		setVisible(true);
+	}
+	
+	public void keyPressed(KeyEvent e){
+		if(e.getKeyCode()==KeyEvent.VK_ENTER){
+			input.setEditable(false);
+			//-----grab quote-----------
+			String quote=input.getText();
+			input.setText("");
+			addText("You:\t"+quote);
+			quote=quote.trim();
+			while(
+				quote.charAt(quote.length()-1)=='!' ||
+				quote.charAt(quote.length()-1)=='.' ||
+				quote.charAt(quote.length()-1)=='?'
+			){
+				quote=quote.substring(0,quote.length()-1);
+			}
+			quote=quote.trim();
+			byte response=0;
+			/*
+			0:we're searching through chatBot[][] for matches
+			1:we didn't find anything
+			2:we did find something
+			*/
+			//-----check for matches----
+			int j=0;//which group we're checking
+			while(response==0){
+				if(inArray(quote.toLowerCase(),chatBot[j*2])){
+					response=2;
+					int r=(int)Math.floor(Math.random()*chatBot[(j*2)+1].length);
+					addText("\nBot:\t"+chatBot[(j*2)+1][r]);
+				}
+				j++;
+				if(j*2==chatBot.length-1 && response==0){
+					response=1;
+				}
+			}
+			
+			//-----default--------------
+			if(response==1){
+				int r=(int)Math.floor(Math.random()*chatBot[chatBot.length-1].length);
+				addText("\nBot:\t"+chatBot[chatBot.length-1][r]);
+			}
+			addText("\n");
+		}
+	}
+	
+	public void keyReleased(KeyEvent e){
+		if(e.getKeyCode()==KeyEvent.VK_ENTER){
+			input.setEditable(true);
+		}
+	}
+	
+	public void keyTyped(KeyEvent e){}
+	
+	public void addText(String str){
+		dialog.setText(dialog.getText()+str);
+	}
+	
+	public boolean inArray(String in,String[] str){
+		boolean match=false;
+		for(int i=0;i<str.length;i++){
+			if(str[i].equals(in)){
+				match=true;
+			}
+		}
+		return match;
+	}
+}
